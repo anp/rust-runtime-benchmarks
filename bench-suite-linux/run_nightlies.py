25abaf2e5e461d78b2de6c52d97d5694ac6e4b40
Adam Perry <adam.n.perry@gmail.com>
2016-03-17 15:06:07 -0700

    Updating bench runner to use perf.

--- bench-suite-linux/run_nightlies.py
+++ bench-suite-linux/run_nightlies.py
@@ -9,16 +9,17 @@ FORMAT = '%Y-%m-%d'
 current = dt.datetime(2015, 8, 20)
 print('start:', current.strftime(FORMAT))
 
-end = dt.datetime(2016, 2, 20)
+end = dt.datetime(2016, 3, 15)
 print('end:', end.strftime(FORMAT))
 
-results_file = '/home/adam/rust_projects/rust-runtime-benchmarks/getrusage-benchmarks.json'
+results_file = '/home/adam/rust_projects/rust-runtime-benchmarks/perf-benchmarks.json'
 
 results = {}
 
 while current <= end:
     date_str = current.strftime(FORMAT)
     toolchain_str = 'nightly-{}'.format(date_str)
+    results[date_str] = {}
 
     print('getting toolchain', toolchain_str)
     cmd = ['multirust', 'update', toolchain_str]
@@ -46,18 +47,55 @@ while current <= end:
         continue
 
     print('running benches for', date_str)
-    completed = sp.run(['target/release/bench-suite-linux'], universal_newlines=True, stderr=sp.PIPE, stdout=sp.PIPE)
-    try:
-        completed.check_returncode()
-    except sp.CalledProcessError as e:
-        print('WARNING: benches failed to run on', toolchain_str)
-        print(completed.stdout, '\n', completed.stderr)
-        current += dt.timedelta(days=1)
-        continue
+    benches = ['cbor'
+               'crc',
+               'csv',
+               'itertools',
+               'memchr',
+               'optional',
+               'permutohedron',
+               'rand',
+               'regex',
+               'suffix',
+               'uuid']
 
-    print('parsing benchmark output...')
-    single_result = json.loads(completed.stdout)
-    results[date_str] = single_result
+    for b in benches:
+        cmd = ['perf', 'stat', '-x,', 'target/release/bench-suite-linux', b]
+        completed = sp.run(cmd, universal_newlines=True, stderr=sp.STDOUT, stdout=sp.PIPE)
+        try:
+            completed.check_returncode()
+        except sp.CalledProcessError as e:
+            print('WARNING: benches failed to run on', toolchain_str)
+            print(completed.stdout, '\n', completed.stderr)
+            current += dt.timedelta(days=1)
+            continue
+
+        print('parsing benchmark output...')
+        output = completed.stdout.split('\n')
+
+        # get the json results from getrusage and average it
+        single_result = json.loads(output[0])[b]
+        for k in single_result:
+            single_result[k] = sum(single_result[k]) / len(single_result[k])
+
+        # get the line-by-line results from perf
+        for l in output[1:]:
+            tokens = [t for t in l.split(',') if len(t) > 0]
+
+            if len(tokens) < 2:
+                continue
+
+            if tokens[0] == '<not supported>':
+                continue
+
+            if '.' in tokens[0]:
+                count = float(tokens[0])
+            else:
+                count = int(tokens[0])
+
+            single_result[tokens[1]] = count
+
+        results[date_str][b] = single_result
 
     print('moving to next day')
     current += dt.timedelta(days=1)
--- bench-suite-linux/src/main.rs
+++ bench-suite-linux/src/main.rs
@@ -1,7 +1,8 @@
-#![feature(test)]
+#![feature(convert, test)]
 
 extern crate nix;
-#[macro_use] extern crate lazy_static;
+#[macro_use]
+extern crate lazy_static;
 extern crate libc;
 extern crate num;
 extern crate rustc_serialize;
@@ -20,8 +21,8 @@ mod crc_bench;
 extern crate csv;
 mod csv_bench;
 
-//extern crate hyper;
-//mod hyper_bench;
+// extern crate hyper;
+// mod hyper_bench;
 
 extern crate itertools;
 mod itertools_bench;
@@ -29,9 +30,9 @@ mod itertools_bench;
 extern crate memchr;
 mod memchr_bench;
 
-//#[macro_use(s)]
-//extern crate ndarray;
-//mod ndarray_bench;
+// #[macro_use(s)]
+// extern crate ndarray;
+// mod ndarray_bench;
 
 extern crate optional;
 mod optional_bench;
@@ -46,8 +47,8 @@ extern crate regex;
 extern crate regex_syntax;
 mod regex_bench;
 
-//extern crate simd;
-//mod simd_bench;
+// extern crate simd;
+// mod simd_bench;
 
 extern crate suffix;
 mod suffix_bench;
@@ -56,6 +57,7 @@ extern crate uuid;
 mod uuid_bench;
 
 use std::collections::BTreeMap;
+use std::env::args;
 
 // borrowing example from hwloc's repository
 fn main() {
@@ -64,17 +66,29 @@ fn main() {
     nix::sched::sched_setaffinity(0, &cpu_set).unwrap();
 
     let mut results: BTreeMap<String, BTreeMap<String, Vec<u64>>> = BTreeMap::new();
-    results.insert("cbor".to_string(), cbor_bench::run_all());
-    results.insert("crc".to_string(), crc_bench::run_all());
-    results.insert("csv".to_string(), csv_bench::run_all());
-    results.insert("itertools".to_string(), itertools_bench::run_all());
-    results.insert("memchr".to_string(), memchr_bench::run_all());
-    results.insert("optional".to_string(), optional_bench::run_all());
-    results.insert("permutohedron".to_string(), permutohedron_bench::run_all());
-    results.insert("rand".to_string(), rand_bench::run_all());
-    results.insert("regex".to_string(), regex_bench::run_all());
-    results.insert("suffix".to_string(), suffix_bench::run_all());
-    results.insert("uuid".to_string(), uuid_bench::run_all());
+
+    // skip the first argument, probably the program name
+    let mut args = args();
+    args.next();
+    for arg in args {
+        let arg = arg.as_str();
+        let _ = match arg {
+            "cbor" => results.insert("cbor".to_string(), cbor_bench::run_all()),
+            "crc" => results.insert("crc".to_string(), crc_bench::run_all()),
+            "csv" => results.insert("csv".to_string(), csv_bench::run_all()),
+            "itertools" => results.insert("itertools".to_string(), itertools_bench::run_all()),
+            "memchr" => results.insert("memchr".to_string(), memchr_bench::run_all()),
+            "optional" => results.insert("optional".to_string(), optional_bench::run_all()),
+            "permutohedron" => {
+                results.insert("permutohedron".to_string(), permutohedron_bench::run_all())
+            }
+            "rand" => results.insert("rand".to_string(), rand_bench::run_all()),
+            "regex" => results.insert("regex".to_string(), regex_bench::run_all()),
+            "suffix" => results.insert("suffix".to_string(), suffix_bench::run_all()),
+            "uuid" => results.insert("uuid".to_string(), uuid_bench::run_all()),
+            _ => panic!("invalid benchmark selected"),
+        };
+    }
 
     println!("{}", serde_json::to_string(&results).unwrap());
 }
